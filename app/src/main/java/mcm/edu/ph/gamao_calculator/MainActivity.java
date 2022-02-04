e428d8fe07cd38bfb88b472679e3dd31953ebc13
JayGamao <jonreff.gamao@yahoo.com>
2022-02-04 12:22:36 +0800

    Commit

--- app/src/main/java/mcm/edu/ph/gamao_calculator/MainActivity.java
+++ app/src/main/java/mcm/edu/ph/gamao_calculator/MainActivity.java
@@ -3,12 +3,80 @@ package mcm.edu.ph.gamao_calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    @Override
+    EditText txtVar1, txtVar2;
+    Button btnAdd, btnSubtract, btnMultiply, btnDivide, answer;
+    TextView result;
+
+    double var1,var2;
+
+
+
+    @Override {
+        btnAdd = findViewById(R.id.btnAdd);
+        btnSubtract = findViewById(R.id.btnSubtract);
+        btnMultiply = findViewById(R.id.btnMultiply);
+        btnDivide = findViewById(R.id.btnDivide);
+        answer = findViewById(R.id.answer);
+
+        txtVar1= findViewById(R.id.FirstVar);
+        txtVar2= findViewById(R.id.SecondVar);
+
+        result = findViewById(R.id.Result);
+
+        btnAdd.setOnClickListener(this);
+        btnSubtract.setOnClickListener(this);
+        btnDivide.setOnClickListener(this);
+        btnMultiply.setOnClickListener(this);
+        answer.setOnClickListener(this);
+
+
+    }
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    @Override
+    public void onClick(View v) {
+
+        txtVar1 = findViewById(R.id.FirstVar);
+        txtVar2 = findViewById(R.id.SecondVar);
+
+        var1 = Double.parseDouble(txtVar1.getText().toString());
+        var2 = Double.parseDouble(txtVar2.getText().toString());
+
+        switch (v.getId()) {
+            case R.id.btnAdd:
+                result.setText(Double.toString(var1+var2));
+
+                break,
+            case R.id.btnSubtract:
+                result.setText(Double.toString(var1-var2));
+
+                break,
+            case R.id.btnDivide:
+                result.setText(Double.toString(var1/var2));
+
+                break,
+            case R.id.answer:
+                result.setText(Double.toString(var1%var2));
+
+                break,
+            case R.id.btnMultiply:
+                result.setText(Double.toString(var1*var2));
+
+                break,
+
+        }
+
+    }
+
 }
